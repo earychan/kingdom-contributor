303791d7d252bc4d1ce2abff16e15dcdb83e5058
Ebraj <gurungebraj22@gmail.com>
2021-07-16 22:58:48 +0545

    Header added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.scss";
 
+// Importing the header
+import Header from "./Header";
+
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Header/>
     </div>
   );
 };
